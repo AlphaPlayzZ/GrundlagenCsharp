4d39add21b7d51e845af9cdf436576a471c967e1
AlphaPlayzZ <111435361+AlphaPlayzZ@users.noreply.github.com>
2022-09-07 14:37:15 +0200

    Projekt erstellt

--- 1_uebungen_Verzweigungen/Program.cs
+++ 1_uebungen_Verzweigungen/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("kwugdcvkwgd");
+﻿//Aufgabe 1
+
+
+
+int x = 9,z = 6; double y = 8; 
+
+Console.WriteLine(--x == y);                    //Ausgabe False
+Console.WriteLine(x==(int)y);                   //Ausgabe False
+Console.WriteLine("{0:F1}",0.123);              //Ausgabe 0,1
+Console.WriteLine(z/y);                         //Ausgabe 0,75
+Console.WriteLine(x-=10);                       //Ausgabe -1
+Console.WriteLine(x--== y & true);              //Ausgabe False
+Console.WriteLine(!(z == 6) | false);           //Ausgabe False
+Console.WriteLine((x*=-2) == z );                 //Ausgabe False
+
+
+//Aufgabe 2
+
+/*
+Console.WriteLine("\nBitte eine Zahl eingeben (Celsius oder Kelvin)?: ");
+double Temp = float.Parse(Console.ReadLine());
+Console.WriteLine("Welche einheit  ist ihre Temperatur?" +
+    "\n(1) Umrechnung von Celsius nach Kelvin" +
+    "\n(2) Umrechnung von Celsius nach Fahrenheit " +
+    "\n(3) Umrechnung von Kelvin nach Celsius" +
+    "\n(4) Umrechnung von Kelvin nach Fahrenheit");
+Console.WriteLine("Ihre Auswahl?: ");
+int entscheidung =Convert.ToInt32(Console.ReadLine());
+
+
+switch (entscheidung)
+{
+    case 1:   Console.WriteLine("Bitte eine Celsius eingabe machen: ");
+        double Celsius= float.Parse(Console.ReadLine());
+        Console.WriteLine(Celsius + "°C für fahrenheit entspricht: " + (Celsius * 1.8 + 32));
+        
+*/
+
+
+
+
+
+}
+
