2a05a06207053ebed0c9c0f4ed20f1f69e8ba188
AlphaPlayzZ <111435361+AlphaPlayzZ@users.noreply.github.com>
2022-11-02 14:29:29 +0100

    Übung while

--- 2_Uebung_KA_2/Program.cs
+++ 2_Uebung_KA_2/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Aufgabe 1 
+
+
+/*
+string sHöhe = "Höhe", sBreite = "Breite"; float fHöhe = 32.346F, fBreite = 15.193F;
+
+
+Console.WriteLine("{0,10}{1,10}\n", sHöhe,sBreite);
+
+Console.WriteLine($"{fHöhe,10:F1}{fBreite,10:F1}");
+
+*/
+
+//Aufgabe 5
+
+Console.Write("Bitte geben Sie Ihren Umsatz ein: ");
+double Umsatz= double.Parse(Console.ReadLine());
+
+if (Umsatz < 100)
+
+{
+    Console.WriteLine("\nSie bekommen leider keinen Rabatt.");
+
+}
+
+else if (Umsatz >=100 && Umsatz<=500)
+    {
+
+    Console.WriteLine("\nSie bekommen einen Rabatt von 5%.");
+    double N1Umsatz =(Umsatz * 95)/100;
+    Console.WriteLine();
+    Console.WriteLine(N1Umsatz + " ist ihr neuer Betrag");
+
+}
+
+else if (Umsatz >=500)
+{
+    Console.WriteLine("\nSie bekommen einen Rabatt von 10%");
+
+}
+
+
+
+
---
+++ Do-While Schleifen/Program.cs
@@ -0,0 +1,31 @@
+﻿//Do-While Aufgabe
+
+int Kontostand = 1000;
+
+while (Kontostand >=0)
+
+{
+    Console.WriteLine("Ihr Kontostand beträgt: " + Kontostand + " EUR");
+
+    Kontostand -= 100;
+
+}
+
+
+
+
+Console.WriteLine("Bitte geben Sie Ihren Namen ein: ");
+string Name= Console.ReadLine();
+
+ while (Name == "")
+
+{
+
+    Console.WriteLine("Sie haben eine Falsche Eingabe gemacht!\n");
+    Console.WriteLine("Bitte geben Sie Ihren Namen erneut ein: ");
+    Name = Console.ReadLine();
+
+}
+
+
+
